095cb25d781cd33f9588e4ca699c76ac9f20c0f0
Ciarra Peters <cnoelp@yahoo.com>
2019-01-18 15:24:56 -0500

    Deleted all migrations

--- db/migrate/20181130185848_add_student_to_coop_opportunity.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentToCoopOpportunity < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :coop_positions, :student, foreign_key: true
-  end
-end
--- db/migrate/20181130185938_add_company_to_coop_opportunity.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToCoopOpportunity < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :coop_positions, :company, foreign_key: true
-  end
-end
